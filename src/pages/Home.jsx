2dd83eae1a51da67460ab77905b256e3aefc5a16
Mubiynaxon <166293367+Mubiynaxon@users.noreply.github.com>
2025-03-25 13:20:18 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-
-const HomePage = ({ user, onLogout }) => {
+const HomePage = ({ user }) => {
   const navigate = useNavigate();
-
-  
 
   return (
     <div className="container">
